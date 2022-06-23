c327ceebce7c71b576b34db13596720b7ad09933
cardosofede <federico.cardoso.e@gmail.com>
2022-06-23 19:53:53 -0300

    (fix) replaced local video for YT video

--- 🚀_Home.py
+++ 🚀_Home.py
@@ -10,7 +10,7 @@ st.write("")
 st.write("Watch this video to understand how the dashboard works! 🦅")
 c1, c2, c3 = st.columns([1, 6, 1])
 with c2:
-    st.video("assets/loom_video.mp4")
+    st.video("https://youtu.be/l6PWbN2pDK8")
 st.write("If you want to contribute, post your idea in #dev-channel of [hummingbot discord](https://discord.gg/CjxZtkrH)")
 st.write("---")
 
