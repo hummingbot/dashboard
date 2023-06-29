541b3e79e8ecac1d17b7c3f4218d2965999a160e
cardosofede <federico.cardoso.e@gmail.com>
2023-06-29 20:38:11 +0100

    (feat) refactor analysis page and notebook

--- pages/9_⚙️_Backtesting.py
+++ pages/9_⚙️_Backtesting.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pandas_ta as ta
 import streamlit as st
 
+from quants_lab.strategy.mean_reversion.bollinger import Bollinger
 from quants_lab.utils import data_management
 from quants_lab.backtesting.backtesting import Backtesting
 from quants_lab.backtesting.backtesting_analysis import BacktestingAnalysis
@@ -14,44 +15,37 @@ st.set_page_config(
 )
 st.title("⚙️ Backtesting")
 
-
-df = data_management.get_dataframe(
-    exchange='binance_perpetual',
-    trading_pair='BTC-USDT',
-    interval='1m',
-)
-
 df_to_show = data_management.get_dataframe(
     exchange='binance_perpetual',
-    trading_pair='BTC-USDT',
+    trading_pair='IOTA-USDT',
     interval='1h',
 )
 
-def bbands_strategy(df):
-    df.ta.bbands(length=100, std=3, append=True)
-    df["side"] = 0
-    long_condition = df["BBP_100_3.0"] < -0.2
-    short_condition = df["BBP_100_3.0"] > 1.2
-    df.loc[long_condition, "side"] = 1
-    df.loc[short_condition, "side"] = -1
-    return df
+strategy = Bollinger(
+    exchange="binance_perpetual",
+    trading_pair="ETH-USDT",
+    interval="1h",
+    bb_length=24,
+    bb_std=2.0,
+)
 
-backtesting = Backtesting(candles_df=df)
+backtesting = Backtesting(strategy=strategy)
+
 
 backtesting_result = backtesting.run_backtesting(
-    strategy=bbands_strategy,
     order_amount=50,
     leverage=20,
     initial_portfolio=100,
-    take_profit_multiplier=1.5,
-    stop_loss_multiplier=0.8,
-    time_limit=60 * 60 * 24,
+    take_profit_multiplier=3.5,
+    stop_loss_multiplier=1.5,
+    time_limit=60 * 60 * 12,
     std_span=None,
 )
 
 
 backtesting_analysis = BacktestingAnalysis(df_to_show, backtesting_result, extra_rows=1, show_volume=False)
 backtesting_analysis.add_trade_pnl(row=2)
+# backtesting_analysis.add_positions()
 
 c1, c2 = st.columns([0.2, 0.8])
 with c1:
