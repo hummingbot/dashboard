a0eb7e626935f2905324fe6b7e8cc2ffe41f561a
cardosofede <federico.cardoso.e@gmail.com>
2023-06-29 20:37:30 +0100

    (feat) add bollinger strategy

--- quants_lab/strategy/mean_reversion/bollinger.py
+++ quants_lab/strategy/mean_reversion/bollinger.py
@@ -10,12 +10,16 @@ class Bollinger(DirectionalStrategyBase):
                  trading_pair="ETH-USDT",
                  interval="1h",
                  bb_length=24,
-                 bb_std=2.0):
+                 bb_std=2.0,
+                 bb_long_threshold=0.0,
+                 bb_short_threshold=1.0,):
         self.exchange = exchange
         self.trading_pair = trading_pair
         self.interval = interval
         self.bb_length = bb_length
         self.bb_std = bb_std
+        self.bb_long_threshold = bb_long_threshold
+        self.bb_short_threshold = bb_short_threshold
 
     def get_raw_data(self):
         df = data_management.get_dataframe(
@@ -31,7 +35,7 @@ class Bollinger(DirectionalStrategyBase):
 
     def add_signals(self, df):
         df["side"] = 0
-        long_condition = df[f"BBP_{self.bb_length}_{self.bb_std}"] < 0.05
+        long_condition = df[f"BBP_{self.bb_length}_{self.bb_std}"] < self.bb_long_threshold
         short_condition = df[f"BBP_{self.bb_length}_{self.bb_std}"] > 0.95
         df.loc[long_condition, "side"] = 1
         df.loc[short_condition, "side"] = -1
