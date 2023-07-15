2f7e7f6cf511675d66ec807a05d4d2975d772ae8
drupman <tomasgaudino@hotmail.com>
2023-07-14 23:23:48 -0300

    (feat) strategy_performance page reengineering + candles pagination

--- pages/2_🚀_Strategy_Performance.py
+++ pages/2_🚀_Strategy_Performance.py
@@ -1,9 +1,11 @@
 import os
 import base64
+import math
 
 import pandas as pd
 import streamlit as st
 
+from constants import intervals
 from utils.database_manager import DatabaseManager
 from utils.graphs import CandlesGraph
 
@@ -13,18 +15,20 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed"
 )
-st.title("🚀 Strategy Performance")
 
-intervals = {
-    "1m": 60,
-    "3m": 60 * 3,
-    "5m": 60 * 5,
-    "15m": 60 * 15,
-    "30m": 60 * 30,
-    "1h": 60 * 60,
-    "6h": 60 * 60 * 6,
-    "1d": 60 * 60 * 24,
-}
+
+def sidebar_metrics(strategy_data_filtered):
+    return f"""
+# Market statistics
+EXCHANGE={strategy_data_filtered.exchange.capitalize()}
+TRADING_PAIR={strategy_data_filtered.trading_pair.upper()}
+
+# General stats
+DURATION={round(strategy_data_filtered.duration_seconds / 3600, 2)} HOURS
+START_DATE={strategy_data_filtered.start_time.strftime("%Y-%m-%d %H:%M")}
+END_DATE={strategy_data_filtered.end_time.strftime("%Y-%m-%d %H:%M")}
+PRICE_CHANGE={round(strategy_data_filtered.price_change * 100, 2)} %
+"""
 
 
 def download_csv(dataframe):
@@ -40,90 +44,90 @@ def execute_query(db_manager, query):
 
 
 @st.cache_resource
-def get_database(db_name: str):
-    db_manager = DatabaseManager(db_name)
-    return db_manager
+def get_databases():
+    sqlite_files = [db_name for db_name in os.listdir("data") if db_name.endswith(".sqlite")]
+    databases_list = [DatabaseManager(db) for db in sqlite_files]
+    return {database.db_name: database for database in databases_list}
 
 
-with st.container():
-    col1, col2 = st.columns(2)
+st.title("🚀 Strategy Performance")
+dbs = get_databases()
+db_names = [x.db_name for x in dbs.values() if x.status == 'OK']
+if not db_names:
+    st.warning("No trades have been recorded in the selected database")
+    selected_db_name = None
+    selected_db = None
+else:
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
-        db_names = [db_name for db_name in os.listdir("data") if db_name.endswith(".sqlite")]
-        selected_db_name = st.selectbox("Select a database to use:",
-                                        db_names if len(db_names) > 0 else ["No databases found"])
-    if selected_db_name == "No databases found":
-        st.warning("No databases available to analyze. Please run a backtesting first.")
-    else:
-        db_manager = get_database(selected_db_name)
-        # TODO: Handle corrupt db files
-        config_files = db_manager.get_config_files()
-        if config_files == []:
-            with col1:
-                st.warning('No trades have been recorded in the selected database')
-        with col2:
-            selected_config_file = st.selectbox("Select a config file to analyze:", config_files)
-            if selected_config_file is not None:
-                exchanges_trading_pairs = db_manager.get_exchanges_trading_pairs_by_config_file(selected_config_file)
-                strategy_data = db_manager.get_strategy_data(selected_config_file)
+        selected_db_name = st.selectbox("Select a database to use:", db_names)
+        selected_db = dbs[selected_db_name]
+    with col2:
+        if selected_db:
+            selected_config_file = st.selectbox("Select a config file to analyze:", selected_db.config_files)
+        else:
+            selected_config_file = None
+    with col3:
+        if selected_config_file:
+            selected_exchange = st.selectbox("Exchange:", selected_db.configs[selected_config_file].keys())
+    with col4:
+        if selected_config_file:
+            selected_trading_pair = st.selectbox("Trading Pair:", options=selected_db.configs[selected_config_file].values())
+
+    single_market = True
+    if single_market:
+        strategy_data = dbs[selected_db_name].get_strategy_data(selected_config_file)
+        single_market_strategy_data = strategy_data.get_single_market_strategy_data(selected_exchange, selected_trading_pair)
+        date_array = pd.date_range(start=strategy_data.start_time, end=strategy_data.end_time, periods=60)
+        start_time, end_time = st.select_slider("Select a time range to analyze",
+                                                options=date_array.tolist(),
+                                                value=(date_array[0], date_array[-1]))
+        strategy_data_filtered = single_market_strategy_data.get_filtered_strategy_data(start_time, end_time)
+
+        st.sidebar.code(sidebar_metrics(strategy_data_filtered))
 
         with st.container():
-            col1, col2, col3 = st.columns(3)
+            st.header("📈 Performance")
+            col131, col132, col133, col134 = st.columns(4)
+            with col131:
+                st.metric(label=f'Net PNL {strategy_data_filtered.quote_asset}',
+                          value=round(strategy_data_filtered.net_pnl_quote, 2))
+                st.metric(label=f'Trade PNL {strategy_data_filtered.quote_asset}',
+                          value=round(strategy_data_filtered.trade_pnl_quote, 2))
+                st.metric(label=f'Fees {strategy_data_filtered.quote_asset}',
+                          value=round(strategy_data_filtered.cum_fees_in_quote, 2))
+            with col132:
+                st.metric(label='Total Trades', value=strategy_data_filtered.total_orders)
+                st.metric(label='Total Buy Trades', value=strategy_data_filtered.total_buy_trades)
+                st.metric(label='Total Sell Trades', value=strategy_data_filtered.total_sell_trades)
+            with col133:
+                st.metric(label='Inventory change in Base asset',
+                          value=round(strategy_data_filtered.inventory_change_base_asset, 4))
+                st.metric(label='Total Buy Trades Amount',
+                          value=round(strategy_data_filtered.total_buy_amount, 2))
+                st.metric(label='Total Sell Trades Amount',
+                          value=round(strategy_data_filtered.total_sell_amount, 2))
+            with col134:
+                st.metric(label='End Price', value=round(strategy_data_filtered.end_price, 4))
+                st.metric(label='Average Buy Price', value=round(strategy_data_filtered.average_buy_price, 4))
+                st.metric(label='Average Sell Price', value=round(strategy_data_filtered.average_sell_price, 4))
+
+        if strategy_data_filtered.market_data is not None:
+            col1, col2, col3 = st.columns([1, 1, 2])
             with col1:
-                selected_exchange = st.selectbox("Select an exchange:", [] if selected_config_file is None else list(exchanges_trading_pairs.keys()))
-            with col2:
-                selected_trading_pair = st.selectbox("Select a trading pair:", [] if selected_config_file is None else exchanges_trading_pairs[selected_exchange])
-            with col3:
                 interval = st.selectbox("Candles Interval:", intervals.keys(), index=2)
+            with col2:
+                rows_per_page = st.number_input("Candles per Page", value=100, min_value=1, max_value=5000)
+            with col3:
+                total_rows = len(strategy_data_filtered.get_market_data_resampled(interval=f"{intervals[interval]}S"))
+                total_pages = math.ceil(total_rows / rows_per_page)
+                selected_page = st.select_slider("Select page", list(range(total_pages)), key="page_slider")
 
-        if selected_exchange and selected_trading_pair:
-            single_market_strategy_data = strategy_data.get_single_market_strategy_data(selected_exchange,
-                                                                                        selected_trading_pair)
-            date_array = pd.date_range(start=strategy_data.start_time, end=strategy_data.end_time, periods=60)
-            start_time, end_time = st.select_slider("Select a time range to analyze", options=date_array.tolist(),
-                                                    value=(date_array[0], date_array[-1]))
-
-            strategy_data_filtered = single_market_strategy_data.get_filtered_strategy_data(start_time, end_time)
-            # TODO: Define sidebar metrics vs. st.metrics in main page
-            st.sidebar.code(f"""
-# Market statistics
-EXCHANGE={strategy_data_filtered.exchange.capitalize()}
-TRADING_PAIR={strategy_data_filtered.trading_pair.upper()}
-
-# General stats
-DURATION={round(strategy_data_filtered.duration_seconds / 3600, 2)} HOURS
-START_DATE={strategy_data_filtered.start_time.strftime("%Y-%m-%d %H:%M")}
-END_DATE={strategy_data_filtered.end_time.strftime("%Y-%m-%d %H:%M")}
-PRICE_CHANGE={round(strategy_data_filtered.price_change * 100, 2)} %
-""")
-
-            row = st.container()
-            with row:
-                st.header("📈 Performance")
-                col131, col132, col133, col134 = st.columns(4)
-                with col131:
-                    st.metric(label=f'Net PNL {strategy_data_filtered.quote_asset}',
-                              value=round(strategy_data_filtered.net_pnl_quote, 2))
-                    st.metric(label=f'Trade PNL {strategy_data_filtered.quote_asset}',
-                              value=round(strategy_data_filtered.trade_pnl_quote, 2))
-                    st.metric(label=f'Fees {strategy_data_filtered.quote_asset}',
-                              value=round(strategy_data_filtered.cum_fees_in_quote, 2))
-                with col132:
-                    st.metric(label='Total Trades', value=strategy_data_filtered.total_orders)
-                    st.metric(label='Total Buy Trades', value=strategy_data_filtered.total_buy_trades)
-                    st.metric(label='Total Sell Trades', value=strategy_data_filtered.total_sell_trades)
-                with col133:
-                    st.metric(label='Inventory change in Base asset',
-                              value=round(strategy_data_filtered.inventory_change_base_asset, 4))
-                    st.metric(label='Total Buy Trades Amount',
-                              value=round(strategy_data_filtered.total_buy_amount, 2))
-                    st.metric(label='Total Sell Trades Amount',
-                              value=round(strategy_data_filtered.total_sell_amount, 2))
-                with col134:
-                    st.metric(label='End Price', value=round(strategy_data_filtered.end_price, 4))
-                    st.metric(label='Average Buy Price', value=round(strategy_data_filtered.average_buy_price, 4))
-                    st.metric(label='Average Sell Price', value=round(strategy_data_filtered.average_sell_price, 4))
-            if strategy_data_filtered.market_data is not None:
-                candles_df = strategy_data_filtered.get_market_data_resampled(interval=f"{intervals[interval]}S")
+            if selected_page is not None:
+                start_idx = selected_page * rows_per_page
+                end_idx = start_idx + rows_per_page
+                candles_df = strategy_data_filtered.get_market_data_resampled(interval=f"{intervals[interval]}S").iloc[
+                             start_idx:end_idx]
                 cg = CandlesGraph(candles_df, show_volume=False, extra_rows=2)
                 cg.add_buy_trades(strategy_data_filtered.buys)
                 cg.add_sell_trades(strategy_data_filtered.sells)
@@ -131,9 +135,9 @@ PRICE_CHANGE={round(strategy_data_filtered.price_change * 100, 2)} %
                 cg.add_base_inventory_change(strategy_data_filtered, row=3)
                 fig = cg.figure()
                 st.plotly_chart(fig, use_container_width=True)
-            else:
-                st.warning("Market data is not available so the candles graph is not going to be rendered. "
-                           "Make sure that you are using the latest version of Hummingbot and market data recorder activated.")
+        else:
+            st.warning("Market data is not available so the candles graph is not going to be rendered. "
+                       "Make sure that you are using the latest version of Hummingbot and market data recorder activated.")
 
         # TODO: Avoid reloading all page every time you use this
         with st.container():
@@ -141,8 +145,7 @@ PRICE_CHANGE={round(strategy_data_filtered.price_change * 100, 2)} %
             query = st.text_area("SQL Query")
             run_query = st.button("Run query!")
             if run_query and query is not None:
-                with db_manager.session_maker() as session:
-                    results = execute_query(db_manager, query)
+                results = execute_query(selected_db, query)
                 if results is not None:
                     download_csv(results)
                     st.dataframe(results)
