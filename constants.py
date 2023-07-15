56424e144aaa3f7bd568e785eb9fbc50e05d5d1c
drupman <tomasgaudino@hotmail.com>
2023-07-14 23:14:03 -0300

    (feat) add intervals to constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,13 @@
 CANDLES_DATA_PATH = "data/candles"
 DOWNLOAD_CANDLES_CONFIG_YML = "hummingbot_files/scripts_configs/data_downloader_config.yml"
 BOTS_FOLDER = "hummingbot_files/bot_configs"
+intervals = {
+    "1m": 60,
+    "3m": 60 * 3,
+    "5m": 60 * 5,
+    "15m": 60 * 15,
+    "30m": 60 * 30,
+    "1h": 60 * 60,
+    "6h": 60 * 60 * 6,
+    "1d": 60 * 60 * 24,
+}
