222c3e7f2ec641f0e15bae754f621ab4d43d1f2a
Michael Feng <mifeng@gmail.com>
2023-07-22 09:37:09 -0400

    (feat) initial base page

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import streamlit as st
+from pathlib import Path
+from page import Page
 
-apptitle = "High Frequency Trading"
-st.set_page_config(page_title=apptitle, page_icon="🦅", layout="wide")
+class HummingbotDashboardPage(Page):
+    def __init__(self):
+        super().__init__("Hummingbot Dashboard", Path("README.md").read_text())
 
-st.title("Welcome!")
-st.write("---")
-st.code("💡 The purpose of this dashboard is to provide useful information for high frequency trading traders")
-st.write("")
-st.write("Watch this video to understand how the dashboard works! 🦅")
-c1, c2, c3 = st.columns([1, 6, 1])
-with c2:
-    st.video("https://youtu.be/l6PWbN2pDK8")
-st.write("If you want to contribute, post your idea in #dev-channel of [hummingbot discord](https://discord.gg/CjxZtkrH)")
-st.write("---")
+    def write_page(self):
+        super().write_page()
+        st.code("💡 The purpose of this dashboard is to provide useful information for high frequency trading traders")
+        st.write("")
+        st.write("Watch this video to understand how the dashboard works! 🦅")
+        c1, c2, c3 = st.columns([1, 6, 1])
+        with c2:
+            st.video("https://youtu.be/l6PWbN2pDK8")
+        st.write("If you want to contribute, post your idea in #dev-channel of [hummingbot discord](https://discord.gg/CjxZtkrH)")
 
-
-
-
+if __name__ == "__main__":
+    page = HummingbotDashboardPage()
+    page.write_page()
