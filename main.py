1e4cce67470f1eb90ac747012fd8497c0ee234c1
cardosofede <federico.cardoso.e@gmail.com>
2023-07-24 17:22:56 +0200

    (feat) add initialize st_page to main page

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 import streamlit as st
-from pathlib import Path
 from st_pages import Page, show_pages
 
-title = "Hummingbot Dashboard"
-icon = "📊"
+from utils.st_utils import initialize_st_page
 
-st.set_page_config(
-    page_title=title,
-    page_icon=icon,
-    layout="wide",
-)
-st.title(f"{icon} {title}")
 
-current_directory = Path(__file__).parent
-readme_path = current_directory / "README.md"
-with st.expander("About This Page"):
-    st.write(readme_path.read_text())
+initialize_st_page(title="Hummingbot Dashboard", icon="📊")
 
 show_pages(
     [
