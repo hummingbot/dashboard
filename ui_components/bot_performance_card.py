d07d52e8022eed165500569a22a61854298c44b5
cardosofede <federico.cardoso.e@gmail.com>
2023-07-27 00:19:24 +0200

    (feat) minor improvments in card

--- ui_components/bot_performance_card.py
+++ ui_components/bot_performance_card.py
@@ -17,17 +17,19 @@ class BotPerformanceCard(Dashboard.Item):
                       sx={"display": "flex", "flexDirection": "column", "borderRadius": 3, "overflow": "hidden"},
                       elevation=2):
             color = "green" if bot_config["is_running"] else "red"
+            start_date = bot_config.get("start_date", "Not Available")
             mui.CardHeader(
                 title=bot_config["bot_name"],
-                subheader=f"Running since {bot_config['start_date']}",
+                subheader=f"Running since {start_date}",
                 avatar=mui.Avatar("🤖", sx={"bgcolor": color}),
                 className=self._draggable_class,
             )
 
             with mui.CardContent(sx={"flex": 1}):
-                mui.Typography(bot_config["balances"])
-                mui.Typography(bot_config["trades"])
-                mui.Typography(bot_config["pnl"])
+                mui.Typography("Status:")
+                mui.Typography(bot_config["status"])
+                mui.Typography("Trades:")
+                mui.Typography(str(bot_config["trades"]))
 
             with mui.CardActions(disableSpacing=True):
                 mui.IconButton(mui.icon.Stop)
