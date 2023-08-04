8f2a0e674e0ad6dbfd7d8ccd0cd5869cf5601ed1
cardosofede <federico.cardoso.e@gmail.com>
2023-08-04 18:39:02 +0200

    (feat) add specific file explorers

--- ui_components/directional_strategies_file_explorer.py
+++ ui_components/directional_strategies_file_explorer.py
@@ -8,7 +8,8 @@ from utils.os_utils import get_python_files_from_directory
 class DirectionalStrategiesFileExplorer(FileExplorerBase):
     def add_tree_view(self):
         with mui.lab.TreeView(defaultExpandIcon=mui.icon.ChevronRight, defaultCollapseIcon=mui.icon.ExpandMore,
-                              onNodeSelect=lambda event, node_id: self.set_selected_file(event, node_id)):
+                              onNodeSelect=lambda event, node_id: self.set_selected_file(event, node_id),
+                              defaultExpanded=["directional_strategies"]):
             with mui.lab.TreeItem(nodeId="directional_strategies", label=f"⚔️Directional Strategies"):
                 strategies = get_python_files_from_directory(constants.DIRECTIONAL_STRATEGIES_PATH)
                 for strategy in strategies:
