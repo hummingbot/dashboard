80191d6dc09698b6f10152021b0517cde97f5640
cardosofede <federico.cardoso.e@gmail.com>
2023-09-18 12:06:30 +0800

    (feat) adapt bollinger controllers

--- quants_lab/controllers/bollinger.py
+++ quants_lab/controllers/bollinger.py
@@ -1,36 +1,61 @@
+from typing import Optional
+
+import pandas as pd
 import pandas_ta as ta
-from pydantic import BaseModel, Field
+from hummingbot.smart_components.executors.position_executor.position_executor import PositionExecutor
+from hummingbot.smart_components.strategy_frameworks.data_types import OrderLevel
+from hummingbot.smart_components.strategy_frameworks.directional_trading import DirectionalTradingControllerConfigBase, \
+    DirectionalTradingControllerBase
+from pydantic import Field
 
-from quants_lab.strategy.directional_strategy_base import DirectionalStrategyBase
 
-
-class BollingerConf(BaseModel):
-    exchange: str = Field(default="binance_perpetual")
-    trading_pair: str = Field(default="ETH-USDT")
-    interval: str = Field(default="1h")
+class BollingerConf(DirectionalTradingControllerConfigBase):
+    strategy_name = "bollinger"
     bb_length: int = Field(default=100, ge=2, le=1000)
     bb_std: float = Field(default=2.0, ge=0.5, le=4.0)
     bb_long_threshold: float = Field(default=0.0, ge=-3.0, le=0.5)
     bb_short_threshold: float = Field(default=1.0, ge=0.5, le=3.0)
+    std_span: Optional[int] = Field(default=100, ge=100, le=400)
 
 
-class Bollinger(DirectionalStrategyBase[BollingerConf]):
-    def get_raw_data(self):
-        df = self.get_candles(
-            exchange=self.config.exchange,
-            trading_pair=self.config.trading_pair,
-            interval=self.config.interval,
-        )
-        return df
+class Bollinger(DirectionalTradingControllerBase):
 
-    def preprocessing(self, df):
+    def __init__(self, config: BollingerConf):
+        super().__init__(config)
+        self.config = config
+
+    def early_stop_condition(self, executor: PositionExecutor, order_level: OrderLevel) -> bool:
+        """
+        If an executor has an active position, should we close it based on a condition.
+        """
+        return False
+
+    def cooldown_condition(self, executor: PositionExecutor, order_level: OrderLevel) -> bool:
+        """
+        After finishing an order, the executor will be in cooldown for a certain amount of time.
+        This prevents the executor from creating a new order immediately after finishing one and execute a lot
+        of orders in a short period of time from the same side.
+        """
+        if executor.close_timestamp and executor.close_timestamp + order_level.cooldown_time > time.time():
+            return True
+        return False
+
+    def get_processed_data(self) -> pd.DataFrame:
+        df = self.candles[0].candles_df
+
+        # Add indicators
         df.ta.bbands(length=self.config.bb_length, std=self.config.bb_std, append=True)
-        return df
 
-    def predict(self, df):
-        df["side"] = 0
+        # Generate signal
         long_condition = df[f"BBP_{self.config.bb_length}_{self.config.bb_std}"] < self.config.bb_long_threshold
         short_condition = df[f"BBP_{self.config.bb_length}_{self.config.bb_std}"] > self.config.bb_short_threshold
-        df.loc[long_condition, "side"] = 1
-        df.loc[short_condition, "side"] = -1
+
+        # Generate signal
+        df["signal"] = 0
+        df.loc[long_condition, "signal"] = 1
+        df.loc[short_condition, "signal"] = -1
+
+        # Optional: Generate spread multiplier
+        if self.config.std_span:
+            df["target"] = df["close"].rolling(self.config.std_span).std() / df["close"]
         return df
