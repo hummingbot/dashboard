e766c6cb0a11fc8e07c87d1d49f91adb7448119c
cardosofede <federico.cardoso.e@gmail.com>
2023-10-04 22:49:42 -0300

    (feat) add dman controllers for market making

--- quants_lab/controllers/dman_v2.py
+++ quants_lab/controllers/dman_v2.py
@@ -13,20 +13,20 @@ from hummingbot.smart_components.strategy_frameworks.market_making.market_making
 )
 
 
-class BollinGridConfig(MarketMakingControllerConfigBase):
-    strategy_name: str = "bollinger_grid"
-    bb_length: int = 12
-    bb_long_threshold: float = 0.7
-    bb_short_threshold: float = 0.3
+class DManV2Config(MarketMakingControllerConfigBase):
+    strategy_name: str = "dman_v2"
+    macd_fast: int = 12
+    macd_slow: int = 26
+    macd_signal: int = 9
     natr_length: int = 14
 
 
-class BollinGrid(MarketMakingControllerBase):
+class DManV2(MarketMakingControllerBase):
     """
     Directional Market Making Strategy making use of NATR indicator to make spreads dynamic and shift the mid price.
     """
 
-    def __init__(self, config: BollinGridConfig):
+    def __init__(self, config: DManV2Config):
         super().__init__(config)
         self.config = config
 
@@ -61,18 +61,18 @@ class BollinGrid(MarketMakingControllerBase):
         """
         candles_df = self.candles[0].candles_df
         natr = ta.natr(candles_df["high"], candles_df["low"], candles_df["close"], length=self.config.natr_length) / 100
-        candles_df.ta.bbands(length=self.config.bb_length, std=2, append=True)
-        bbp = candles_df[f"BBP_{self.config.bb_length}_2.0"]
+
+        macd_output = ta.macd(candles_df["close"], fast=self.config.macd_fast, slow=self.config.macd_slow, signal=self.config.macd_signal)
+        macd = macd_output[f"MACD_{self.config.macd_fast}_{self.config.macd_slow}_{self.config.macd_signal}"]
+        macdh = macd_output[f"MACDh_{self.config.macd_fast}_{self.config.macd_slow}_{self.config.macd_signal}"]
+        macd_signal = - (macd - macd.mean()) / macd.std()
+        macdh_signal = macdh.apply(lambda x: 1 if x > 0 else -1)
+        max_price_shift = natr / 2
+
+        price_multiplier = (0.5 * macd_signal + 0.5 * macdh_signal) * max_price_shift
 
         candles_df["spread_multiplier"] = natr
-        candles_df["price_multiplier"] = bbp
-
-        # Generate filter
-        long_condition = (bbp < self.config.bb_long_threshold)
-        short_condition = (bbp > self.config.bb_short_threshold)
-        candles_df["active_side"] = 0
-        candles_df.loc[long_condition, "active_side"] = 1
-        candles_df.loc[short_condition, "active_side"] = -1
+        candles_df["price_multiplier"] = price_multiplier
         return candles_df
 
     def get_position_config(self, order_level: OrderLevel) -> PositionConfig:
@@ -81,32 +81,32 @@ class BollinGrid(MarketMakingControllerBase):
         Here you can use technical indicators to determine the parameters of the position config.
         """
         close_price = self.get_close_price(self.config.exchange, self.config.trading_pair)
-        bbp, spread_multiplier, active_side = self.get_price_and_spread_multiplier()
-        current_side = 1 if order_level.side == TradeType.BUY else -1
-        if active_side == current_side:
-            spread_multiplier = spread_multiplier if order_level.side == TradeType.SELL else -spread_multiplier
-            order_price = close_price * (1 + order_level.spread_factor * spread_multiplier)
-            amount = order_level.order_amount_usd / order_price
-            if order_level.triple_barrier_conf.trailing_stop_trailing_delta and order_level.triple_barrier_conf.trailing_stop_trailing_delta:
-                trailing_stop = TrailingStop(
-                    activation_price_delta=order_level.triple_barrier_conf.trailing_stop_activation_price_delta,
-                    trailing_delta=order_level.triple_barrier_conf.trailing_stop_trailing_delta,
-                )
-            else:
-                trailing_stop = None
-            position_config = PositionConfig(
-                timestamp=time.time(),
-                trading_pair=self.config.trading_pair,
-                exchange=self.config.exchange,
-                side=order_level.side,
-                amount=amount,
-                take_profit=order_level.triple_barrier_conf.take_profit * Decimal(spread_multiplier),
-                stop_loss=order_level.triple_barrier_conf.stop_loss,
-                time_limit=order_level.triple_barrier_conf.time_limit,
-                entry_price=Decimal(order_price),
-                open_order_type=order_level.triple_barrier_conf.open_order_type,
-                take_profit_order_type=order_level.triple_barrier_conf.take_profit_order_type,
-                trailing_stop=trailing_stop,
-                leverage=self.config.leverage
+        amount = order_level.order_amount_usd / close_price
+        price_multiplier, spread_multiplier, side_filter = self.get_price_and_spread_multiplier()
+
+        price_adjusted = close_price * (1 + price_multiplier)
+        side_multiplier = -1 if order_level.side == TradeType.BUY else 1
+        order_price = price_adjusted * (1 + order_level.spread_factor * spread_multiplier * side_multiplier)
+        if order_level.triple_barrier_conf.trailing_stop_trailing_delta and order_level.triple_barrier_conf.trailing_stop_trailing_delta:
+            trailing_stop = TrailingStop(
+                activation_price_delta=order_level.triple_barrier_conf.trailing_stop_activation_price_delta,
+                trailing_delta=order_level.triple_barrier_conf.trailing_stop_trailing_delta,
             )
-            return position_config
+        else:
+            trailing_stop = None
+        position_config = PositionConfig(
+            timestamp=time.time(),
+            trading_pair=self.config.trading_pair,
+            exchange=self.config.exchange,
+            side=order_level.side,
+            amount=amount,
+            take_profit=order_level.triple_barrier_conf.take_profit,
+            stop_loss=order_level.triple_barrier_conf.stop_loss,
+            time_limit=order_level.triple_barrier_conf.time_limit,
+            entry_price=Decimal(order_price),
+            open_order_type=order_level.triple_barrier_conf.open_order_type,
+            take_profit_order_type=order_level.triple_barrier_conf.take_profit_order_type,
+            trailing_stop=trailing_stop,
+            leverage=self.config.leverage
+        )
+        return position_config
