0b823c2c7a5956e2119043001691c1e457d2fd1a
Ralph Comia <rapcmia@mishka.local>
2023-10-20 20:32:54 +0800

    add helpers for adding admin pass and email address to credential.yml

---
+++ helpers/add_authemail.py
@@ -0,0 +1,26 @@
+import streamlit_authenticator as stauth
+import ruamel.yaml as yaml
+import os
+
+# enter email address
+new_email = input("Enter dashboard email >> ") 
+
+# if user enter no email address, exit setup!
+if len(new_email) == 0:
+    print("\nNo email added, please try again!\n")
+    exit()
+
+
+# load the YAML file 
+yaml_file = "../credentials.yml" 
+with open(yaml_file, "r") as file:
+    data = yaml.safe_load(file)
+
+# update the admin password on credentials.yml
+data["preauthorized"]["emails"].append(new_email)
+
+# write the updated data back to the file
+with open(yaml_file, "w") as file:
+    yaml.dump(data, file, Dumper=yaml.RoundTripDumper)
+
+print("Email has been added")
