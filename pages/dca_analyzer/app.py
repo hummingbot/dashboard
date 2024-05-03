cbb0469a545bd79d2ea8e0722527bcc0abf9c253
drupman <tomasgaudino@hotmail.com>
2024-05-03 19:24:52 -0300

    (feat) add tsl_tp_sl_tl_es column + refactor names

--- pages/dca_analyzer/app.py
+++ pages/dca_analyzer/app.py
@@ -17,20 +17,44 @@ from data_viz.tracers import PerformancePlotlyTracer
 load_dotenv()
 
 
-def get_total_and_exit_levels(executors_with_orders: pd.DataFrame, executors: pd.DataFrame):
-    exit_level = executors_with_orders[executors_with_orders["position"] == "OPEN"].groupby("executor_id")["position"].count()
-    executors["exit_level"] = executors["id"].map(exit_level).fillna(0.0).astype(int)
-    executors["total_levels"] = executors["config"].apply(lambda x: len(json.loads(x)["prices"]))
-    return executors
+def concatenate_values(row):
+    tsl = row[row['close_type'] == 'TRAILING_STOP']['count'].values
+    tp = row[row['close_type'] == 'TAKE_PROFIT']['count'].values
+    sl = row[row['close_type'] == 'STOP_LOSS']['count'].values
+    tl = row[row['close_type'] == 'TIME_LIMIT']['count'].values
+    es = row[row['close_type'] == 'EARLY_STOP']['count'].values
 
-def get_executors_with_orders(executors: pd.DataFrame, orders: pd.DataFrame):
-    df = pd.DataFrame(executors['custom_info'].tolist(), index=executors['id'],
-                         columns=["custom_info"]).reset_index()
+    tsl_count = tsl[0] if len(tsl) > 0 else 0
+    tp_count = tp[0] if len(tp) > 0 else 0
+    sl_count = sl[0] if len(sl) > 0 else 0
+    tl_count = tl[0] if len(tl) > 0 else 0
+    es_count = es[0] if len(es) > 0 else 0
+
+    return f"{tsl_count}\t | \t{tp_count}\t | \t{sl_count}\t | \t{tl_count}\t | \t{es_count}"
+
+
+def get_tp_sl_tsl_es_tl(executors_df: pd.DataFrame, grouped_executors_df: pd.DataFrame):
+    df = executors_df.groupby(["instance", "controller_id", "exchange", "trading_pair", "db_name", "close_type"])[
+        "close_type"].count().reset_index(name="count")
+    result = df.groupby(["instance", "controller_id", "exchange", "trading_pair", "db_name"]).apply(concatenate_values).reset_index(name='tsl_tp_sl_tl_es')
+    return grouped_executors_df.merge(result, on=["instance", "controller_id", "exchange", "trading_pair", "db_name"], how="left")
+
+
+def get_total_and_exit_levels(executors_with_orders_df: pd.DataFrame, executors_df: pd.DataFrame):
+    exit_level = executors_with_orders_df[executors_with_orders_df["position"] == "OPEN"].groupby("executor_id")["position"].count()
+    executors_df["exit_level"] = executors_df["id"].map(exit_level).fillna(0.0).astype(int)
+    executors_df["total_levels"] = executors_df["config"].apply(lambda x: len(json.loads(x)["prices"]))
+    return executors_df
+
+
+def get_executors_with_orders(executors_df: pd.DataFrame, orders_df: pd.DataFrame):
+    df = pd.DataFrame(executors_df['custom_info'].tolist(), index=executors_df['id'],
+                      columns=["custom_info"]).reset_index()
     df["custom_info"] = df["custom_info"].apply(lambda x: json.loads(x))
     df["orders"] = df["custom_info"].apply(lambda x: x["order_ids"])
     df.rename(columns={"id": "executor_id"}, inplace=True)
     exploded_df = df.explode("orders").rename(columns={"orders": "order_id"})
-    exec_with_orders = exploded_df.merge(orders, left_on="order_id", right_on="client_order_id", how="inner")
+    exec_with_orders = exploded_df.merge(orders_df, left_on="order_id", right_on="client_order_id", how="inner")
     exec_with_orders = exec_with_orders[exec_with_orders["last_status"].isin(["SellOrderCompleted", "BuyOrderCompleted"])]
     return exec_with_orders[["executor_id", "order_id", "last_status", "last_update_timestamp", "price", "amount", "position"]]
 
@@ -114,9 +138,10 @@ grouped_executors["duration"] = (grouped_executors["close_datetime"] - grouped_e
 grouped_executors["filled_amount_quote"] = grouped_executors["filled_amount_quote"].apply(lambda x: f"$ {x:.2f}")
 grouped_executors["net_pnl_quote"] = grouped_executors["net_pnl_quote"].apply(lambda x: f"$ {x:.2f}")
 grouped_executors["filter"] = False
+grouped_executors = get_tp_sl_tsl_es_tl(executors, grouped_executors)
 grouped_executors.rename(columns={"datetime": "start_datetime_utc",
                                   "id": "total_executors"}, inplace=True)
-cols_to_show = ["filter", "controller_id", "exchange", "trading_pair", "db_name", "total_executors", "filled_amount_quote", "net_pnl_quote", "duration"]
+cols_to_show = ["filter", "controller_id", "exchange", "trading_pair", "db_name", "total_executors", "filled_amount_quote", "net_pnl_quote", "duration", "tsl_tp_sl_tl_es"]
 selection = st.data_editor(grouped_executors[cols_to_show], use_container_width=True, hide_index=True, column_config={"filter": st.column_config.CheckboxColumn(required=True)})
 with st.expander("🔍 Filters"):
     col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
@@ -252,8 +277,6 @@ fig.update_layout(
 )
 
 st.plotly_chart(fig, use_container_width=True)
-
-
 
 # Apply custom sorting function to create a new column 'sorting_order'
 level_id_data[['type', 'number']] = level_id_data['level_id'].str.split('_', expand=True)
