d93dd73f9db492cab9ce5f811cd726e8de145b7e
cardosofede <federico.cardoso.e@gmail.com>
2024-05-18 03:17:26 -0400

    (feat) refactor deploy pages

--- frontend/pages/orchestration/launch_bot_v2/app.py
+++ frontend/pages/orchestration/launch_bot_v2/app.py
@@ -1,4 +1,10 @@
-from frontend.components.deploy_v2_with_controllers import LaunchV2WithControllers
+from types import SimpleNamespace
+
+import streamlit as st
+from streamlit_elements import elements, mui
+
+from frontend.components.dashboard import Dashboard
+from frontend.components.launch_strategy_v2 import LaunchStrategyV2
 from frontend.st_utils import initialize_st_page
 
 CARD_WIDTH = 6
@@ -7,6 +13,19 @@ NUM_CARD_COLS = 2
 
 initialize_st_page(title="Launch Bot", icon="🙌")
 
+if "launch_bots_board" not in st.session_state:
+    board = Dashboard()
+    launch_bots_board = SimpleNamespace(
+        dashboard=board,
+        launch_bot=LaunchStrategyV2(board, 0, 0, 12, 10),
+    )
+    st.session_state.launch_bots_board = launch_bots_board
 
-launcher = LaunchV2WithControllers()
-launcher()
+else:
+    launch_bots_board = st.session_state.launch_bots_board
+
+
+with elements("create_bot"):
+    with mui.Paper(elevation=3, style={"padding": "2rem"}, spacing=[2, 2], container=True):
+        with launch_bots_board.dashboard():
+            launch_bots_board.launch_bot()
