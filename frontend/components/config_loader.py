9cb1a9cf064f4cd69ed1cf77f2a317683b40d8d5
cardosofede <federico.cardoso.e@gmail.com>
2024-06-06 23:49:30 +0200

    (feat) restore it if is a different controller name

--- frontend/components/config_loader.py
+++ frontend/components/config_loader.py
@@ -12,7 +12,8 @@ def get_default_config_loader(controller_name: str):
     existing_configs = [config["id"].split("-")[0] for config in all_configs]
     default_dict = {"id": generate_random_name(existing_configs)}
     default_config = st.session_state.get("default_config")
-    config_controller_name = st.session_state.get("controller_name", "nan")
+    config_controller_name = st.session_state.get("controller_name", controller_name)
+    st.write(f"controller_name: {controller_name} | config_controller_name: {config_controller_name}")
     if default_config is None or controller_name != config_controller_name:
         st.session_state["default_config"] = default_dict
     with st.expander("Configurations", expanded=True):
