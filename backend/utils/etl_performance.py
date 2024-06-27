a8d0b8732dd484efb08675f4fa75ca0e351136f2
drupman <tomasgaudino@hotmail.com>
2024-06-27 20:30:44 -0300

    (feat) extremely simplify hummingbot_database parsing and concatenating + remove market data from market recorder + delegate metrics calculation to etl_performance

--- backend/utils/etl_performance.py
+++ backend/utils/etl_performance.py
@@ -1,5 +1,7 @@
 import json
+from typing import List
 
+from hummingbot.core.data_type.common import TradeType
 from hummingbot.strategy_v2.models.base import RunnableStatus
 from hummingbot.strategy_v2.models.executors import CloseType
 from sqlalchemy import create_engine, delete, insert, text, MetaData, Table, Column, VARCHAR, INT, FLOAT, TIMESTAMP,  Integer, String, Float, DateTime
@@ -20,30 +22,13 @@ class ETLPerformance:
         self.metadata = MetaData()
 
     @property
-    def market_data_table(self):
-        return Table(
-            'market_data', MetaData(),
-            Column('timestamp', TIMESTAMP),
-            Column('trading_pair', VARCHAR),
-            Column('exchange', VARCHAR),
-            Column('mid_price', FLOAT),
-            Column('best_bid', FLOAT),
-            Column('best_ask', FLOAT),
-            Column('order_book', VARCHAR),
-            Column('instance', VARCHAR),
-            Column('db_path', VARCHAR),
-            Column('db_name', VARCHAR)
-        )
-
-    @property
     def executors_table(self):
         return Table('executors',
                      MetaData(),
                      Column('id', String),
+                     Column('timestamp', Integer),
                      Column('type', String),
                      Column('close_type', Integer),
-                     Column('level_id', String),
-                     Column('timestamp', Integer),
                      Column('close_timestamp', Integer),
                      Column('status', String),
                      Column('config', String),
@@ -54,20 +39,7 @@ class ETLPerformance:
                      Column('is_active', Integer),
                      Column('is_trading', Integer),
                      Column('custom_info', String),
-                     Column('controller_id', String),
-                     Column('datetime', DateTime),
-                     Column('close_datetime', DateTime),
-                     Column('trading_pair', String),
-                     Column('exchange', String),
-                     Column('side', String),
-                     Column('bep', Float),
-                     Column('close_price', Float),
-                     Column('sl', Float),
-                     Column('tp', Float),
-                     Column('tl', Float),
-                     Column('instance', String),
-                     Column('db_path', String),
-                     Column('db_name', String))
+                     Column('controller_id', String))
 
     @property
     def trade_fill_table(self):
@@ -86,24 +58,11 @@ class ETLPerformance:
             Column('price', FLOAT),
             Column('amount', FLOAT),
             Column('leverage', INT),
-            Column('trade_fee', FLOAT),
+            Column('trade_fee', VARCHAR(255)),
             Column('trade_fee_in_quote', FLOAT),
             Column('exchange_trade_id', VARCHAR(255)),
             Column('position', VARCHAR(255)),
-            Column('cum_fees_in_quote', FLOAT),
-            Column('net_amount', FLOAT),
-            Column('net_amount_quote', FLOAT),
-            Column('cum_net_amount', FLOAT),
-            Column('unrealized_trade_pnl', FLOAT),
-            Column('inventory_cost', FLOAT),
-            Column('realized_trade_pnl', FLOAT),
-            Column('net_realized_pnl', FLOAT),
-            Column('realized_pnl', FLOAT),
-            Column('gross_pnl', FLOAT),
-            Column('quote_volume', FLOAT),
-            Column('instance', VARCHAR(255)),
-            Column('db_path', VARCHAR(255)),
-            Column('db_name', VARCHAR(255)))
+        )
 
     @property
     def orders_table(self):
@@ -125,14 +84,11 @@ class ETLPerformance:
             Column('last_update_timestamp', TIMESTAMP),
             Column('exchange_order_id', VARCHAR(255)),
             Column('position', VARCHAR(255)),
-            Column('instance', VARCHAR(255)),
-            Column('db_path', VARCHAR(255)),
-            Column('db_name', VARCHAR(255))
         )
 
     @property
     def tables(self):
-        return [self.executors_table, self.market_data_table, self.trade_fill_table, self.orders_table]
+        return [self.executors_table, self.trade_fill_table, self.orders_table]
 
     def list_tables(self):
         with self.session_maker() as session:
@@ -167,8 +123,6 @@ class ETLPerformance:
     def insert_data(self, data):
         if "executors" in data:
             self.insert_executors(data["executors"])
-        if "market_data" in data:
-            self.insert_market_data(data["market_data"])
         if "trade_fill" in data:
             self.insert_trade_fill(data["trade_fill"])
         if "orders" in data:
@@ -178,11 +132,9 @@ class ETLPerformance:
         with self.engine.connect() as conn:
             for _, row in executors.iterrows():
                 ins = self.executors_table.insert().values(
-                    id=row["id"],
+                    timestamp=row["timestamp"],
                     type=row["type"],
                     close_type=row["close_type"],
-                    level_id=row["level_id"],
-                    timestamp=row["timestamp"],
                     close_timestamp=row["close_timestamp"],
                     status=row["status"],
                     config=row["config"],
@@ -193,46 +145,33 @@ class ETLPerformance:
                     is_active=row["is_active"],
                     is_trading=row["is_trading"],
                     custom_info=row["custom_info"],
-                    controller_id=row["controller_id"],
-                    datetime=row["datetime"],
-                    close_datetime=row["close_datetime"],
-                    trading_pair=row["trading_pair"],
-                    exchange=row["exchange"],
-                    side=row["side"],
-                    bep=row["bep"],
-                    close_price=row["close_price"],
-                    sl=row["sl"],
-                    tp=row["tp"],
-                    tl=row["tl"],
-                    instance=row["instance"],
-                    db_path=row["db_path"],
-                    db_name=row["db_name"]
-                )
+                    controller_id=row["controller_id"])
                 conn.execute(ins)
+                conn.commit()
 
-    def read_executors(self):
+    def load_executors(self):
         with self.session_maker() as session:
             query = "SELECT * FROM executors"
             executors = pd.read_sql_query(text(query), session.connection())
-            executors["close_datetime"] = pd.to_datetime(executors["close_datetime"])
-            executors["datetime"] = pd.to_datetime(executors["timestamp"], unit="s")
+            executors["datetime"] = pd.to_datetime(executors.timestamp, unit="s")
+            executors["close_datetime"] = pd.to_datetime(executors["close_timestamp"], unit="s")
+            executors["status"] = executors["status"].apply(lambda x: self.get_enum_by_value(RunnableStatus, int(x)))
+            executors["trading_pair"] = executors["config"].apply(lambda x: json.loads(x)["trading_pair"])
+            executors["exchange"] = executors["config"].apply(lambda x: json.loads(x)["connector_name"])
+            executors["side"] = executors["config"].apply(lambda x: self.get_enum_by_value(TradeType, int(json.loads(x)["side"])))
             executors["close_type"] = executors["close_type"].apply(lambda x: self.get_enum_by_value(CloseType, int(x)))
             executors["close_type_name"] = executors["close_type"].apply(lambda x: x.name)
-            executors["status"] = executors["status"].apply(lambda x: self.get_enum_by_value(RunnableStatus, int(x)))
+            executors["level_id"] = executors["config"].apply(lambda x: json.loads(x).get("level_id") if json.loads(x).get("level_id") is not None else 0)
+            executors["bep"] = executors["custom_info"].apply(lambda x: json.loads(x)["current_position_average_price"])
+            executors["close_price"] = executors["custom_info"].apply(lambda x: json.loads(x)["close_price"])
+            executors["sl"] = executors["config"].apply(lambda x: json.loads(x)["stop_loss"]).fillna(0)
+            executors["tp"] = executors["config"].apply(lambda x: json.loads(x)["take_profit"]).fillna(0)
+            executors["tl"] = executors["config"].apply(lambda x: json.loads(x)["time_limit"]).fillna(0)
+            executors = executors[~executors["close_timestamp"].isna()]
             return executors
 
     @staticmethod
-    def get_enum_by_value(enum_class, value):
-        for member in enum_class:
-            if member.value == value:
-                return member
-        raise ValueError(f"No enum member with value {value}")
-
-    @staticmethod
-    def _parse_executors(executors):
-        """
-        ExecutorInfo(id='2Qd1k5ALefhJ9kfeHYEE4THCG2HpX9ETvogg6rebpYfB', timestamp=1717763580.0, type='position_executor', close_timestamp=1717767240.0, close_type=<CloseType.TAKE_PROFIT: 3>, status=<RunnableStatus.TERMINATED: 4>, config=PositionExecutorConfig(id='2Qd1k5ALefhJ9kfeHYEE4THCG2HpX9ETvogg6rebpYfB', type='position_executor', timestamp=1717763580.0, controller_id='main', trading_pair='WLD-USDT', connector_name='kucoin', side=<TradeType.BUY: 1>, entry_price=Decimal('4.8345'), amount=Decimal('41.36932464577516'), triple_barrier_config=TripleBarrierConfig(stop_loss=Decimal('0.05'), take_profit=Decimal('0.02'), time_limit=43200, trailing_stop=TrailingStop(activation_price=Decimal('0.018'), trailing_delta=Decimal('0.002')), open_order_type=<OrderType.MARKET: 1>, take_profit_order_type=<OrderType.LIMIT: 2>, stop_loss_order_type=<OrderType.MARKET: 1>, time_limit_order_type=<OrderType.MARKET: 1>), leverage=20, activation_bounds=None, level_id=None), net_pnl_pct=Decimal('0.02072603164753395'), net_pnl_quote=Decimal('4.14520632950679'), cum_fees_quote=Decimal('0.1'), filled_amount_quote=Decimal('200'), is_active=False, is_trading=False, custom_info={'close_price': 4.9347, 'level_id': None, 'side': 1, 'current_position_average_price': 4.8345}, controller_id=None)
-        """
+    def parse_executors(executors):
         executor_values = []
         for _, row in executors.iterrows():
             executor_values.append(ExecutorInfo(
@@ -250,36 +189,10 @@ class ETLPerformance:
                 is_active=row["is_active"],
                 is_trading=row["is_trading"],
                 custom_info=json.loads(row["custom_info"]),
-                controller_id=row["controller_id"]
+                controller_id=row["controller_id"],
+                side=row["side"],
             ))
         return executor_values
-
-
-
-    def insert_market_data(self, market_data):
-        market_data.reset_index(inplace=True)
-        with self.engine.connect() as conn:
-            for _, row in market_data.iterrows():
-                ins = insert(self.market_data_table).values(
-                    timestamp=row["timestamp"],
-                    trading_pair=row["trading_pair"],
-                    exchange=row["exchange"],
-                    mid_price=row["mid_price"],
-                    best_bid=row["best_bid"],
-                    best_ask=row["best_ask"],
-                    order_book=row["order_book"],
-                    instance=row["instance"],
-                    db_path=row["db_path"],
-                    db_name=row["db_name"]
-                )
-                conn.execute(ins)
-
-    def read_market_data(self):
-        with self.session_maker() as session:
-            query = "SELECT * FROM market_data"
-            market_data = pd.read_sql_query(text(query), session.connection())
-            market_data["timestamp"] = pd.to_datetime(market_data["timestamp"])
-            return market_data
 
     def insert_trade_fill(self, trade_fill):
         with self.engine.connect() as conn:
@@ -302,24 +215,11 @@ class ETLPerformance:
                     trade_fee_in_quote=row["trade_fee_in_quote"],
                     exchange_trade_id=row["exchange_trade_id"],
                     position=row["position"],
-                    cum_fees_in_quote=row["cum_fees_in_quote"],
-                    net_amount=row["net_amount"],
-                    net_amount_quote=row["net_amount_quote"],
-                    cum_net_amount=row["cum_net_amount"],
-                    unrealized_trade_pnl=row["unrealized_trade_pnl"],
-                    inventory_cost=row["inventory_cost"],
-                    realized_trade_pnl=row["realized_trade_pnl"],
-                    net_realized_pnl=row["net_realized_pnl"],
-                    realized_pnl=row["realized_pnl"],
-                    gross_pnl=row["gross_pnl"],
-                    quote_volume=row["quote_volume"],
-                    instance=row["instance"],
-                    db_path=row["db_path"],
-                    db_name=row["db_name"]
                 )
                 conn.execute(ins)
+                conn.commit()
 
-    def read_trade_fill(self):
+    def load_trade_fill(self):
         with self.session_maker() as session:
             query = "SELECT * FROM trades"
             trade_fill = pd.read_sql_query(text(query), session.connection())
@@ -345,14 +245,19 @@ class ETLPerformance:
                     last_update_timestamp=row["last_update_timestamp"],
                     exchange_order_id=row["exchange_order_id"],
                     position=row["position"],
-                    instance=row["instance"],
-                    db_path=row["db_path"],
-                    db_name=row["db_name"]
                 )
                 conn.execute(ins)
+                conn.commit()
 
-    def read_orders(self):
+    def load_orders(self):
         with self.session_maker() as session:
             query = "SELECT * FROM orders"
             orders = pd.read_sql_query(text(query), session.connection())
             return orders
+
+    @staticmethod
+    def get_enum_by_value(enum_class, value):
+        for member in enum_class:
+            if member.value == value:
+                return member
+        raise ValueError(f"No enum member with value {value}")
--- backend/utils/hummingbot_database.py
+++ backend/utils/hummingbot_database.py
@@ -17,13 +17,6 @@ class HummingbotDatabase:
         self.session_maker = sessionmaker(bind=self.engine)
 
     @staticmethod
-    def load_data(table_loader):
-        try:
-            return table_loader()
-        except Exception as e:
-            return None  # Return None to indicate failure
-
-    @staticmethod
     def _get_table_status(table_loader):
         try:
             data = table_loader()
@@ -50,87 +43,9 @@ class HummingbotDatabase:
                   }
         return status
 
-    @property
-    def config_files(self):
-        return self.get_config_files()
-
-    @property
-    def configs(self):
-        return {config_file: self.get_exchanges_trading_pairs_by_config_file(config_file) for config_file in self.config_files}
-
-    def get_config_files(self):
-        with self.session_maker() as session:
-            query = 'SELECT DISTINCT config_file_path FROM TradeFill'
-            config_files = pd.read_sql_query(text(query), session.connection())
-        return config_files['config_file_path'].tolist()
-
-    def get_exchanges_trading_pairs_by_config_file(self, config_file_path):
-        with self.session_maker() as session:
-            query = f"SELECT DISTINCT market, symbol FROM TradeFill WHERE config_file_path = '{config_file_path}'"
-            exchanges_trading_pairs = pd.read_sql_query(text(query), session.connection())
-            exchanges_trading_pairs["market"] = exchanges_trading_pairs["market"].apply(
-                lambda x: x.lower().replace("_papertrade", ""))
-            exchanges_trading_pairs = exchanges_trading_pairs.groupby("market")["symbol"].apply(list).to_dict()
-        return exchanges_trading_pairs
-
-    @staticmethod
-    def _get_orders_query(config_file_path=None, start_date=None, end_date=None):
-        query = "SELECT * FROM 'Order'"
-        conditions = []
-        if config_file_path:
-            conditions.append(f"config_file_path = '{config_file_path}'")
-        if start_date:
-            conditions.append(f"created_at >= '{start_date}'")
-        if end_date:
-            conditions.append(f"created_at <= '{end_date}'")
-        if conditions:
-            query += f" WHERE {' AND '.join(conditions)}"
-        return query
-
-    @staticmethod
-    def _get_order_status_query(order_ids=None, start_date=None, end_date=None):
-        query = "SELECT * FROM OrderStatus"
-        conditions = []
-        if order_ids:
-            order_ids_string = ",".join(f"'{order_id}'" for order_id in order_ids)
-            conditions.append(f"order_id IN ({order_ids_string})")
-        if start_date:
-            conditions.append(f"created_at >= '{start_date}'")
-        if end_date:
-            conditions.append(f"created_at <= '{end_date}'")
-        if conditions:
-            query += f" WHERE {' AND '.join(conditions)}"
-        return query
-
-    @staticmethod
-    def _get_trade_fills_query(config_file_path=None, start_date=None, end_date=None):
-        query = "SELECT * FROM TradeFill"
-        conditions = []
-        if config_file_path:
-            conditions.append(f"config_file_path = '{config_file_path}'")
-        if start_date:
-            conditions.append(f"created_at >= '{start_date}'")
-        if end_date:
-            conditions.append(f"created_at <= '{end_date}'")
-        if conditions:
-            query += f" WHERE {' AND '.join(conditions)}"
-        return query
-
-    @staticmethod
-    def _get_executors_query(start_date=None, end_date=None):
-        query = "SELECT * FROM Executors"
-        conditions = []
-        if start_date:
-            conditions.append(f"timestamp >= '{start_date}'")
-        if end_date:
-            conditions.append(f"timestamp <= '{end_date}'")
-        if conditions:
-            query += f" WHERE {' AND '.join(conditions)}"
-        return query
-
     def get_orders(self, config_file_path=None, start_date=None, end_date=None):
         with self.session_maker() as session:
-            query = self._get_orders_query(config_file_path, start_date, end_date)
+            query = "SELECT * FROM 'Order'"
             orders = pd.read_sql_query(text(query), session.connection())
             orders["market"] = orders["market"]
             orders["amount"] = orders["amount"] / 1e6
@@ -143,7 +58,7 @@ class HummingbotDatabase:
         groupers = ["config_file_path", "market", "symbol"]
         float_cols = ["amount", "price", "trade_fee_in_quote"]
         with self.session_maker() as session:
-            query = self._get_trade_fills_query(config_file_path, start_date, end_date)
+            query = "SELECT * FROM TradeFill"
             trade_fills = pd.read_sql_query(text(query), session.connection())
             trade_fills[float_cols] = trade_fills[float_cols] / 1e6
             trade_fills["cum_fees_in_quote"] = trade_fills.groupby(groupers)["trade_fee_in_quote"].cumsum()
@@ -164,24 +79,12 @@ class HummingbotDatabase:
 
     def get_order_status(self, order_ids=None, start_date=None, end_date=None):
         with self.session_maker() as session:
-            query = self._get_order_status_query(order_ids, start_date, end_date)
+            query = "SELECT * FROM OrderStatus"
             order_status = pd.read_sql_query(text(query), session.connection())
         return order_status
 
     def get_executors_data(self, start_date=None, end_date=None) -> pd.DataFrame:
         with self.session_maker() as session:
-            query = self._get_executors_query(start_date, end_date)
+            query = "SELECT * FROM Executors"
             executors = pd.read_sql_query(text(query), session.connection())
-            executors["datetime"] = pd.to_datetime(executors.timestamp, unit="s")
-            executors["close_datetime"] = pd.to_datetime(executors["close_timestamp"], unit="s")
-            executors["trading_pair"] = executors["config"].apply(lambda x: json.loads(x)["trading_pair"])
-            executors["exchange"] = executors["config"].apply(lambda x: json.loads(x)["connector_name"])
-            executors["side"] = executors["config"].apply(lambda x: json.loads(x)["side"])
-            executors["level_id"] = executors["config"].apply(lambda x: json.loads(x).get("level_id") if json.loads(x).get("level_id") is not None else 0)
-            executors["bep"] = executors["custom_info"].apply(lambda x: json.loads(x)["current_position_average_price"])
-            executors["close_price"] = executors["custom_info"].apply(lambda x: json.loads(x)["close_price"])
-            executors["sl"] = executors["config"].apply(lambda x: json.loads(x)["stop_loss"]).fillna(0)
-            executors["tp"] = executors["config"].apply(lambda x: json.loads(x)["take_profit"]).fillna(0)
-            executors["tl"] = executors["config"].apply(lambda x: json.loads(x)["time_limit"]).fillna(0)
-            executors = executors[~executors["close_timestamp"].isna()]
         return executors
